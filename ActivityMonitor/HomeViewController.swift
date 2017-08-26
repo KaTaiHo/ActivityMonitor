a988530842e8d828bad5c3ed9e5a364f0ee78277
warrior5715 <kataihoo@gmail.com>
2017-08-25 22:33:03 -0500

    added some code for login

--- ActivityMonitor/HomeViewController.swift
+++ ActivityMonitor/HomeViewController.swift
@@ -13,12 +13,21 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var _username: UITextField!
     @IBOutlet weak var _password: UITextField!
     @IBOutlet weak var _login_button: UIButton!
-    @IBOutlet weak var _register_button: UIButton!
+//    @IBOutlet weak var _register_button: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let preferences = UserDefaults.standard
+        
+        if (preferences.object(forKey: "session") != nil)
+        {
+            LoginDone()
+        }
+        else
+        {
+            LoginToDo()
+        }
         // Do any additional setup after loading the view.
     }
 
@@ -29,7 +38,92 @@ class HomeViewController: UIViewController {
     
     
     @IBAction func LoginButton(_ sender: Any) {
+        
+        if(_login_button.titleLabel?.text == "Logout")
+        {
+            let preferneces = UserDefaults.standard
+            preferneces.removeObject(forKey: "session")
+            
+            LoginToDo()
+            return
+        }
+        
+        let username = _username.text
+        let password = _password.text
+        
+        if (username == "" || password == "") {
+            return
+        }
+        
+        DoLogin(username!, password!)
     }
 
+//    @IBAction func RegisterButton(_ sender: Any) {
+//        return
+//    }
+    
+    func DoLogin(_ user:String, _ psw:String) {
+        let url = URL(string: "http://127.0.0.1:5000/")
+        let session = URLSession.shared
+        
+        let request = NSMutableURLRequest(url: url!)
+        request.httpMethod = "POST"
+        
+        let paramToSend = "username=" + user + "&password=" + psw
+        
+        request.httpBody = paramToSend.data(using: String.Encoding.utf8)
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {data,response,error in
+            guard let _:Data = data
+            else
+            {
+                return
+            }
+            let json:Any?
+            do
+            {
+                json = try JSONSerialization.jsonObject(with: data!, options: [])
+            }
+            catch
+            {
+                return
+            }
+            
+            guard let server_response = json as? NSDictionary else
+            {
+                return
+            }
+            
+            if let data_block = server_response["data"] as? NSDictionary
+            {
+                if  let session_data = data_block["session"] as? String
+                {
+                    let preferences = UserDefaults.standard
+                    preferences.set(session_data, forKey: "session")
+                    
+                    DispatchQueue.main.async {
+                        self.LoginDone()
+                    }
+                }
+            }
+            
+        })
+        
+        task.resume()
+    }
+    
+    func LoginToDo()
+    {
+        _username.isEnabled = true
+        _password.isEnabled = true
+        _login_button.setTitle("Login", for: .normal)
+    }
+    
+    func LoginDone()
+    {
+        _username.isEnabled = false
+        _password.isEnabled = false
+        _login_button.setTitle("Logout", for: .normal)
+    }
 
 }
