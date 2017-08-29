9f47360fb0a7fd3b62fa564be41fc91b071e236b
warrior5715 <kataihoo@gmail.com>
2017-08-29 13:52:37 -0500

    commiting pods install for firebase

--- ActivityMonitor/HomeViewController.swift
+++ ActivityMonitor/HomeViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var _username: UITextField!
+    
     @IBOutlet weak var _password: UITextField!
+    
     @IBOutlet weak var _login_button: UIButton!
+    
+    @IBOutlet weak var signInSelector: UISegmentedControl!
+    
+    var isSignIn:Bool = true
     
     override func loadView() {
         super.loadView()
@@ -24,109 +32,57 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let preferences = UserDefaults.standard
-        
-        if (preferences.object(forKey: "session") != nil)
-        {
-            LoginDone()
-        }
-        else
-        {
-            LoginToDo()
-        }
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    
-    @IBAction func LoginButton(_ sender: Any) {
+    @IBAction func signInSelectorChanged(_ sender: UISegmentedControl) {
+        isSignIn = !isSignIn
         
-        if(_login_button.titleLabel?.text == "Logout")
-        {
-            let preferneces = UserDefaults.standard
-            preferneces.removeObject(forKey: "session")
-            
-            LoginToDo()
-            return
+        if isSignIn {
+            _login_button.setTitle("Sign In", for: .normal)
         }
-        
-        let username = _username.text
-        let password = _password.text
-        
-        if (username == "" || password == "") {
-            return
+        else {
+            _login_button.setTitle("Register", for: .normal)
         }
-        
-        DoLogin(username!, password!)
-    }
-    
-    func DoLogin(_ user:String, _ psw:String) {
-        let url = URL(string: "http://127.0.0.1:5000/login")
-        let session = URLSession.shared
-        
-        let request = NSMutableURLRequest(url: url!)
-        request.httpMethod = "POST"
-        
-        let paramToSend = "username=" + user + "&password=" + psw
-        
-        request.httpBody = paramToSend.data(using: String.Encoding.utf8)
-        
-        let task = session.dataTask(with: request as URLRequest, completionHandler: {data,response,error in
-            guard let _:Data = data
-            else
-            {
-                return
-            }
-            let json:Any?
-            do
-            {
-                json = try JSONSerialization.jsonObject(with: data!, options: [])
-            }
-            catch
-            {
-                return
-            }
-            
-            guard let server_response = json as? NSDictionary else
-            {
-                return
-            }
-            
-            if let data_block = server_response["data"] as? NSDictionary
-            {
-                if  let session_data = data_block["session"] as? String
-                {
-                    let preferences = UserDefaults.standard
-                    preferences.set(session_data, forKey: "session")
-                    
-                    DispatchQueue.main.async {
-                        self.LoginDone()
-                    }
-                }
-            }
-            
-        })
-        
-        task.resume()
-    }
-    
-    func LoginToDo()
-    {
-        _username.isEnabled = true
-        _password.isEnabled = true
-        _login_button.setTitle("Login", for: .normal)
-    }
-    
-    func LoginDone()
-    {
-        _username.isEnabled = false
-        _password.isEnabled = false
-        _login_button.setTitle("Logout", for: .normal)
     }
 
+    @IBAction func signInButtonTapped(_ sender: UIButton) {
+        // TODO: form validation
+        if let email = _username.text, let pass = _password.text {
+            if isSignIn {
+                FIRAuth.auth()?.signIn(withEmail: email, password: pass,
+                                       completion: {(user, error) in
+                    if let u = user {
+                        self.performSegue(withIdentifier: "goToData", sender: self)
+                        
+                    }
+                    else {
+                        
+                    }
+                    
+                })
+                
+            }
+            else {
+                FIRAuth.auth()?.createUser(withEmail: email, password: pass, completion: {
+                    (user, error) in
+                    if let u = user {
+                        //go to home screen
+                        self.performSegue(withIdentifier: "goToData", sender: self)
+                    }
+                    else {
+                        //error
+                    }
+                })
+            }
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        _username.resignFirstResponder()
+        _password.resignFirstResponder()
+    }
 }
